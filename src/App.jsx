99b7c46429d7df4633fde1e300fba0b8daf5a863
quantitychar <olenabogoslavska7@gmail.com>
2025-04-10 21:52:36 +0100

    chore: Add basic structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ArtBorddddd from "./containers/ArtBord/ArtBord";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ArtBorddddd />
+    </>
+  );
+}
+
+export default App;
---
+++ src/containers/ArtBord/ArtBord.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtBord = () => {
+  return <div className="art-bord_wrapped">art-bord_wrapped</div>;
+};
+
+export default ArtBord;
